58ba3cb898a2a0fd4938124f9626332f4965a565
cristian.lastrucci <cristian.lastrucci@gmail.com>
2018-05-30 11:54:54 +0200

    inizializzato progetto maven seal

---
+++ seal/src/main/java/clast/seal/App.java
@@ -0,0 +1,13 @@
+package clast.seal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
