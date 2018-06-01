557e74588d9d8299e68bf4d2048ae907e96586d2
cristian.lastrucci <cristian.lastrucci@gmail.com>
2018-06-01 11:15:32 +0200

    aggiunti campi utente. rifattorizzata AdminDaoTest

--- seal/src/main/java/clast/seal/core/model/User.java
+++ seal/src/main/java/clast/seal/core/model/User.java
@@ -1,16 +1,69 @@
 package clast.seal.core.model;
 
 public class User {
-	
+
 	private final String username;
-	
+	private String password;
+	private String name;
+	private String lastname;
+	private String email;
+	private String phone;
+
 	public User(String username) {
-		super();
 		this.username = username;
+	}
+
+	public User(String username, String password, String name, String lastname, String email, String phone) {
+		this.username = username;
+		this.password = password;
+		this.name = name;
+		this.lastname = lastname;
+		this.email = email;
+		this.phone = phone;
 	}
 
 	public String getUsername() {
 		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
 	}
 
 	@Override
@@ -37,7 +90,5 @@ public class User {
 			return false;
 		return true;
 	}
-	
-	
 
 }
--- seal/src/test/java/clast/seal/core/dao/AdminDaoTest.java
+++ seal/src/test/java/clast/seal/core/dao/AdminDaoTest.java
@@ -22,92 +22,113 @@ import clast.seal.core.model.User;
 import nl.jqno.equalsverifier.EqualsVerifier;
 
 public class AdminDaoTest {
-	
-	AdminDao adminDao;
-	
-	SealDb db;
-	
-	Set<User> users;
-	
-	User user1;
+
+	private AdminDao adminDao;
+	private SealDb db;
+	private Set<User> users;
 	
 	@Before
 	public void setUp() {
-		
+
 		users = new HashSet<User>();
-		
+
 		db = mock(SealDb.class);
 		when(db.getAllUsers()).thenReturn(users);
 		when(db.addUser(any(User.class))).thenAnswer(new Answer<Boolean>() {
 			public Boolean answer(InvocationOnMock invocation) throws Throwable {
-				return !users.contains(invocation.getArgumentAt(0, User.class));
+				return users.add(invocation.getArgumentAt(0, User.class));
 			}
 		});
-		
+
 		adminDao = new AdminDao(db);
 	}
-	
+
 	@Test
 	public void testUserCreation() {
-		assertEquals( 0, users.size() );
+		assertEquals(0, users.size());
 
-		User user1 = createUser("user1");
-		assertTrue( adminDao.addUser(user1) );
-		users.add( user1 );
-		verify( db, times(1) ).addUser(user1);
+		User user1 = createUser("user1", "pwd1", "name1", "lastname1", "email1", "phone1");
+		assertTrue(adminDao.addUser(user1));
+		verify(db, times(1)).addUser(user1);
 		assertEquals(1, users.size());
-		
-		User user2 = createUser("user2");
-		assertTrue( adminDao.addUser(user2) );
-		verify( db, times(1) ).addUser(user1);
-		users.add( user2 );
+		verifyUserFields(adminDao.getAllUsers().iterator().next(), "user1", "pwd1", "name1", "lastname1", "email1", "phone1");
+
+		User user2 = createUserStepByStep("user2", "pwd2", "name2", "lastname2", "email2", "phone2");
+		assertTrue(adminDao.addUser(user2));
+		verify(db, times(1)).addUser(user1);
 		assertEquals(2, users.size());
-		
+		verifyUserFields(adminDao.getAllUsers().iterator().next(), "user2", "pwd2", "name2", "lastname2", "email2", "phone2");
+
 		User user3 = createUser("user1");
-		assertFalse( adminDao.addUser(user3) );
-		verify( db, times(2) ).addUser(user3);
-		users.add( user3 );
-		assertEquals( 2, users.size() );
+		assertFalse(adminDao.addUser(user3));
+		verify(db, times(2)).addUser(user3);
+		assertEquals(2, users.size());
 	}
 
 	@Test
 	public void testNoUsers() {
-		assertEquals( 0, adminDao.getAllUsers().size() );
+		assertEquals(0, adminDao.getAllUsers().size());
 	}
-	
+
 	@Test
 	public void testOneUser() {
-		users.add( createUser("user1") );
-		assertEquals( 1, adminDao.getAllUsers().size() );
-		verify( db, times(1) ).getAllUsers();
-		assertEquals("user1", adminDao.getAllUsers().iterator().next().getUsername() );
+		adminDao.addUser(createUser("user1"));
+		assertEquals(1, adminDao.getAllUsers().size());
+		verify(db, times(1)).getAllUsers();
 	}
-	
+
 	@Test
 	public void testMoreUsers() {
-		assertEquals( 0, adminDao.getAllUsers().size() );
-		verify( db, times(1) ).getAllUsers();
-		
-		users.add( createUser("user1") );
-		assertEquals( 1, adminDao.getAllUsers().size() );
-		verify( db, times(2) ).getAllUsers();
-		
-		users.add( createUser("user2") );
-		assertEquals( 2, adminDao.getAllUsers().size() );
-		verify( db, times(3) ).getAllUsers();
-		
-		users.add( createUser("user3") );
-		assertEquals( 3, adminDao.getAllUsers().size() );
-		verify( db, times(4) ).getAllUsers();
+		assertEquals(0, adminDao.getAllUsers().size());
+		verify(db, times(1)).getAllUsers();
+
+		adminDao.addUser(createUser("user1"));
+		assertEquals(1, adminDao.getAllUsers().size());
+		verify(db, times(2)).getAllUsers();
+
+		adminDao.addUser(createUser("user2"));
+		assertEquals(2, adminDao.getAllUsers().size());
+		verify(db, times(3)).getAllUsers();
+
+		adminDao.addUser(createUser("user3"));
+		assertEquals(3, adminDao.getAllUsers().size());
+		verify(db, times(4)).getAllUsers();
 	}
-	
+
 	@Test
 	public void testEqualUsers() {
-		EqualsVerifier.forClass(User.class).verify();
+		// @formatter:off
+		EqualsVerifier.forClass(User.class)
+						.withIgnoredFields("password", "name", "lastname", "email", "phone")
+							.verify();
+		// @formatter:on
 	}
-	
+
 	private User createUser(String username) {
 		return new User(username);
+	}
+
+	private User createUser(String username, String password, String name, String lastname, String email, String phone) {
+		return new User(username, password, name, lastname, email, phone);
+	}
+
+	private User createUserStepByStep(String username, String password, String name, String lastname, String email, String phone) {
+		User user = createUser(username);
+		user.setPassword(password);
+		user.setName(name);
+		user.setLastname(lastname);
+		user.setEmail(email);
+		user.setPhone(phone);
+		return user;
+	}
+
+	private void verifyUserFields(User user, String username, String password, String name, String lastname, String email, String phone) {
+		assertEquals(username, user.getUsername());
+		assertEquals(password, user.getPassword());
+		assertEquals(name, user.getName());
+		assertEquals(lastname, user.getLastname());
+		assertEquals(email, user.getEmail());
+		assertEquals(phone, user.getPhone());
 	}
 
 }
